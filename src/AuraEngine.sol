20f4e2236a032b37b07a5ad1082e83d38f3aaed1
DivyanshTech06 <divyanshtech06@gmail.com>
2025-08-07 21:57:42 +0530

    create healthFactor Calculation and OrableLib.sol

--- src/AuraEngine.sol
+++ src/AuraEngine.sol
@@ -26,8 +26,16 @@ pragma solidity ^0.8.30;
 import {AuraCoin} from "src/AuraCoin.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {AggregatorV3Interface} from "@chainlink-brownie/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {OracleLib} from "./libraries/OracleLib.sol";
+// import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 /**
+ * @dev Always follow the CEI method while writing any function -->
+ * -Check
+ * -Effects (updating the state)
+ * -Interactions
+ *
  * @title AuraEngine
  * @author Divyansh Audichya
  * @notice This Engine will be the Owner of the AuraCoin contract .
@@ -51,6 +59,12 @@ contract AuraEngine is ReentrancyGuard {
     error AuraEngine__MustBeMoreThanZero();
     error AuraEngine__CollateralNotDeposited();
     error AuraEngine__MintingFailed();
+    error AuraEngine__HealthFactorBroken();
+
+    /*///////////////////////////////////////
+                    TYPES
+    ///////////////////////////////////////*/
+    using OracleLib for AggregatorV3Interface;
 
     /*///////////////////////////////////////
                 STATE VARIABLES
@@ -62,6 +76,10 @@ contract AuraEngine is ReentrancyGuard {
 
     mapping(address user => uint256 amountDepositedInEth) s_userToCollateralDepositedInEth;
     mapping(address user => uint256 tokensMinted) s_userToAuraCoinMinted;
+
+    uint8 constant THRESHOLD_HEALTH_FACTOR = 2;
+    uint256 constant PRECISION_FACTOR = 1e18;
+    uint256 constant PRECISION_PRICE_FEED = 1e10;
 
     /*///////////////////////////////////////
                      EVENTS
@@ -144,21 +162,59 @@ contract AuraEngine is ReentrancyGuard {
         nonReentrant
     {
         s_userToAuraCoinMinted[msg.sender] += _amountAuraToMint;
-        _revertIfHealthFactorIsBroken();
+        _revertIfHealthFactorIsBroken(msg.sender);
         bool success = i_auraCoin.mint(msg.sender, _amountAuraToMint);
         if (!success) {
             revert AuraEngine__MintingFailed();
         }
     }
 
-    function _revertIfHealthFactorIsBroken() internal {}
+    /**
+     * @dev THRESHOLD_HEALTH_FACTOR is uint8 and when multiplied by uint256 returns uint256.
+     */
+    function _revertIfHealthFactorIsBroken(address _user) internal view {
+        uint256 healthFactor = getHealthFactor(_user);
+        if (healthFactor < THRESHOLD_HEALTH_FACTOR * PRECISION_FACTOR) {
+            revert AuraEngine__HealthFactorBroken();
+        }
+    }
 
-    function _getPriceInINR(uint256 _amountInEth) internal returns (uint256) {}
+    /**
+     * @dev This one is bit tricky:
+     *
+     * Example->
+     * ALICE-->2 eth==> 2*1e18 1 eth== 1000INR*1e18*2(getINRforETH)
+     *         500*1e18
+     *         healthFactor=2000e18*1e18/500e18=>4 e18
+     *
+     * @dev in order to not losse precision ,we can't return healthFactor in uint8 as then if suppose there is something like 2000/700,it will losse precision
+     * @param _user User for which the health factor is being checked.
+     * @return uint256 it returns 4 * 1e18 in case of above example.
+     */
+    function getHealthFactor(address _user) public view returns (uint256) {
+        uint256 collateralDepositedInEth = getCollateralDepositedByUsers(_user);
+        uint256 amountOfAuraCoinMintedInINR = getAuraCoinMintedByUsers(_user);
+        uint256 collateralDepositedInINR = getINRforEth(collateralDepositedInEth);
+
+        uint256 healthFactor = collateralDepositedInINR * PRECISION_FACTOR / amountOfAuraCoinMintedInINR;
+        return healthFactor;
+    }
+
+    /**
+     * @param _amountInEth Amount in ETH to convert in INR
+     * @return uint256 if _amountInEth = 2 ether and price of 1 eth in INR is 1000INR, then this returns 2000*1e18;
+     */
+    function getINRforEth(uint256 _amountInEth) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(getEthInrPriceFeed());
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData(); // this return value in 8 decimal places ,we need to add 10 more
+        uint256 adjustedPrice = uint256(price) * PRECISION_PRICE_FEED;
+        uint256 finalValue = adjustedPrice * _amountInEth / PRECISION_FACTOR;
+        return finalValue;
+    }
 
     /*///////////////////////////////////////
               GETTER FUNCTIONS
     ///////////////////////////////////////*/
-
     /**
      * @return address Address of the AuraCoin Contract
      */
